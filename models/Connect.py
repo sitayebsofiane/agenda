23a6f38ea11a944c1040eec91944ea99979baa0c
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-02-03 19:46:23 +0100

    add class Connect

---
+++ models/Connect.py
@@ -0,0 +1,10 @@
+class Connect:
+
+
+    """ connect to bdd in postgreSQL"""
+    def __init__(self,bdd="ecole",user="postgres",password="as122014",host="localhost",port="5432"):
+        try:
+            self.con=psycopg2.connect(database=bdd,user=user,password=password,host=host,port=port)
+            self.curseur=None
+        except(Exception ,psycopg2.Error):
+            print("erreur while connecting to postgresSQL")
