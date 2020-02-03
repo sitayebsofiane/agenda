3f5127418dd880f6b43fb94e759c6acb63fd3de2
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-02-03 15:57:48 +0100

    add entitie Events

---
+++ models/entities/Events.py
@@ -0,0 +1,14 @@
+class Events:
+
+    """ constructor of Events """
+    def __init__(self,title,date,description):
+        self.id_event=0
+        self.title=title
+        self.date=date
+        self.description=description
+
+    """ method to return houre """
+    def houre(self,date):
+
+        pass
+    
