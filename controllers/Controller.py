937bcbde0c62a8e02b931db5a975113735a8f0ae
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-02-05 21:51:05 +0100

    create class Controller

---
+++ controllers/Controller.py
@@ -0,0 +1,10 @@
+from getpass import getpass
+
+class Controller:
+
+    def __init__(self,model,view):
+        self.model = model
+        self.view = view
+
+    def login(self,name,first_name,password):
+        pass
