73cad9f7861e36047e551b86f0d12e36c79f9361
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-02-09 15:01:17 +0100

    method check_date check date and titre for the method post

--- controllers/Controller.py
+++ controllers/Controller.py
@@ -13,12 +13,21 @@ class Controller:
         houre = int(input('nouvelle houre: '))
         month = int(input('nouveu mois: '))
         day = int(input('nouveu jour: '))
+        title_existe = False
         date_enter = datetime.datetime(datetime.date.today().year,month,day,houre,minute,0)
         for row in self.model.title_date_of_events():
-            if title == row[0] or date_enter == row[1]:
-                print(' date ou titre existe en base !')
+            #I check if at a distance of 15 minutes there is no meeting return false if is in this case
+            check_date = (date_enter.year == row[1].year and 
+            date_enter.month == row[1].month and date_enter.hour == row[1].hour and date_enter.second == 0
+            and row[1].minute-15 <= date_enter.minute and date_enter.minute <= row[1].minute+15)
+            if  check_date:
+                print('date existe en base choisir un autre ! ')
                 return False,
-        return True,title,date_enter
+            if row[0] == title:
+                title_existe = True
+        if title_existe:
+            print('le titre existe en base')
+        return True,title_existe,title,date_enter
 
 
     """ login with role """
@@ -40,9 +49,9 @@ class Controller:
             print ('vous êtes: ',self.model.role_name(auth[2]),'si vous êtes USER vous avez des droit restreint ')
             try:
                 check = self.check_title_date()
-                if check[0]:
-                    title = check[1]
-                    date = check[2]
+                if check[0] and not check[1]:
+                    title = check[2]
+                    date = check[3]
                     if self.model.role_name(auth[2]) == "ADMIN":
                         description=input('description: ')
                         self.model.post_event(auth[1],title,date,description)
@@ -58,8 +67,8 @@ class Controller:
             try:
                 check = self.check_title_date()
                 if(check[0]):
-                    title = check[1]
-                    date = check[2]
+                    title = check[2]
+                    date = check[3]
                     new_title = input('nouveau titre: ')
                     if self.model.role_name(auth[2]) == "ADMIN":
                         new_desc = input('nouvelle description: ')
